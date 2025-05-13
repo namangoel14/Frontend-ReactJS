5899914b048d14fb6116ef97789eb0544977516a
naman goel <naman.goel@amantyatech.com>
2025-05-13 07:09:01 +0530

    App header implemented

--- App.js
+++ App.js
@@ -15,6 +15,49 @@ import ReactDOM from 'react-dom/client';
  * - Address
  * - Contact
  */
+
+// Header Component for our Food App
+
+const Header = () => {
+  return (
+    <div className="header">
+      {/* Logo */}
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEGhTionX-UL2clxRFmubGb_LHiqLTooE0XQ&s"
+        />
+      </div>
+      <div className="mood">
+        <b>Happy Gut Happy You</b>
+      </div>
+      {/* Navigation Items */}
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About US</li>
+          <li>Sign in</li>
+          <li>
+            <img
+              className="cart"
+              src="https://cdn-icons-png.freepik.com/256/14090/14090797.png?ga=GA1.1.243617205.1746776182&semt=ais_hybrid"
+            />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+// Top level Component for our food App
+const LayoutApp = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render();
+root.render(<LayoutApp />);
--- Foundation.js
+++ Foundation.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { jsx } from 'react/jsx-runtime';
 
+// React.createElement => ReactElement-JS Object =>HTMLElement(render)
+const heading = React.createElement(
+  'h1',
+  { id: 'heading' },
+  'Core React heading'
+);
+
+console.log(heading);
 // JSX (transpiled before it render the JS)- PARCEL - BABEL
 
 // JSX => React.createElement => ReactElement-JS Object =>HTMLElement(render)
